6159d44d368f5985a2beb1a8e31692cc753cd05d
Issa Casandra Mapanao <152985868+cassymapanao@users.noreply.github.com>
2024-12-11 20:29:34 +0800

    Add files via upload

---
+++ admin_page.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang=eng>
+	<head>
+		<title>Admin - Website ni Mapanao</title>
+		<meta charset=utf-8>
+		<meta name="viewport" content="width=device-width, inital-scale=1.0">
+		<link rel="stylesheet" href="css/include.css">
+	</head>
+	<body>
+		<div id="container">
+		<?php include('header.php');?>
+		<?php include('nav3.php');?>
+		
+			<center>
+			<div id="content">
+				<h2>Weekly Report</h2>
+				<img src="dashboard.gif" alt="Dashboard" width="1300" height="650">
+			</div>
+			</center>
+		</div>
+		<br><br><br><br><br><br>
+		<?php include('info-col.php');?>
+		<?php include('footer.php');?>
+	</body>
+</html>
